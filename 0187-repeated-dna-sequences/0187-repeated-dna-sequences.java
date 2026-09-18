478cd82ea6a86a82e880e0bc67db9984ef6a1ed4
Hemant Kumar <hemantraj595@gmail.com>
2026-09-19 01:06:28 +0530

    Time: 561 ms (11.79%), Space: 142.3 MB (58.66%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,18 +1,52 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
 
-        HashSet<String> seen = new HashSet<>();
-        HashSet<String> result = new HashSet<>();
+        if(s.length() < 10) {
+            return new ArrayList<>();
+        }
 
-        for(int i = 0; i <= s.length() - 10; i++) {
+        Set<Integer> seen = new HashSet<>();
+        Set<String> result = new HashSet<>();
 
-            String sub = s.substring(i, i + 10);
+        int k = 10;
+        int rep = 0;
 
-            if(seen.contains(sub)) {
-                result.add(sub);
-            } else {
-                seen.add(sub);
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
+
+        // First window
+        for(int i = 0; i < k; i++) {
+            int pow = k - i - 1;
+
+            rep = rep +
+                  (int)Math.pow(4, pow) *
+                  map.get(s.charAt(i));
+        }
+
+        seen.add(rep);
+
+        // Rolling hash
+        for(int i = k; i < s.length(); i++) {
+
+            // Remove outgoing character
+            rep = rep -
+                  (int)Math.pow(4, k - 1) *
+                  map.get(s.charAt(i-k));
+
+            // Shift
+            rep = 4 * rep;
+
+            // Add incoming character
+            rep = rep + map.get(s.charAt(i));
+
+            if(seen.contains(rep)) {
+                result.add(s.substring(i-k+1, i+1));
             }
+
+            seen.add(rep);
         }
 
         return new ArrayList<>(result);
